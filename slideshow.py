c20fdbed0d481c33a12b693a5c14e746ae51d986
Thomas Steinbinder <thomas.steinbinder@onlinehome.de>
2020-07-21 00:33:43 +0200

    Added fading and endless sliding
    Implemented endless sliding through all images.
    Implemented fading between images with configurable display and fading time

--- slideshow.py
+++ slideshow.py
@@ -32,15 +32,42 @@ def frameImage(img):
            xOffset:img.shape[1] + xOffset] = img
     return newImg
 
-
-images = glob.glob("D:\\testImg\\*.jpg")
-#images = glob.glob("C:\\Users\\Thomas\\Desktop\\photoframe\\*.jpg")
 cv2.namedWindow('window', cv2.WINDOW_NORMAL)
 cv2.setWindowProperty('window', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+#images = glob.glob("D:\\testImg\\2\\*.jpg")
+images = glob.glob("C:\\Users\\Thomas\\Desktop\\photoframe\\*.jpg")
 
-for i in range(0, len(images)):
-    image = cv2.imread(images[i])
-    image = resizeImage(image)
-    image = frameImage(image)
-    cv2.imshow("window", image)
-    cv2.waitKey(0)
+image1 = cv2.imread(images[0])
+image1 = resizeImage(image1)
+image1 = frameImage(image1)
+cv2.imshow("window", image1)
+
+i = 1;
+count = len(images)
+displayTime = 2000
+fadeTime = 3000
+fadeSteps = int(fadeTime / 100)
+timePerFadeStep = int(fadeTime / fadeSteps)
+
+while True:
+    if i == count:
+        i = 0
+        
+    image2 = cv2.imread(images[i])
+    image2 = resizeImage(image2)
+    image2 = frameImage(image2)
+    
+    cv2.waitKey(displayTime)
+    
+    for a in range(fadeSteps - 1, 0, -1):
+        alpha = a / int(fadeTime / 100)
+        blendImg = image1
+        beta = ( 1.0 - alpha )
+        cv2.addWeighted(image1, alpha, image2, beta, 0.0, blendImg)
+        cv2.imshow("window", blendImg)
+        cv2.waitKey(timePerFadeStep)
+        
+    image1 = image2
+    cv2.imshow("window", image1)
+    #cv2.waitKey(0)
+    i += 1
