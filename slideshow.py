c9e0aeb32a73039b9473b91317b00c4e698211aa
Thomas Steinbinder <thomas.steinbinder@onlinehome.de>
2020-07-21 11:52:54 +0200

    Code cleanup + PEP 8 compliant
    Cleaned up code.
    Made it mire PEP 8 compliant (variable names etc.)

--- slideshow.py
+++ slideshow.py
@@ -5,65 +5,76 @@
 # Released under GNU Public License (GPL)
 # -----------------------------------------------------------
 
-import ctypes
 import glob
 import cv2
 import numpy as np
 
-def resizeImage(img):
-    widthFactor = sWidth / img.shape[1]
-    heightFactor = sHeight / img.shape[0]
-    resizeFactor = widthFactor
-    if heightFactor < widthFactor:
-        resizeFactor = heightFactor
-    newImg = cv2.resize(img, (0, 0), fx=resizeFactor, fy=resizeFactor)
-    return newImg
 
-def frameImage(img):
-    newImg = np.zeros((sHeight, sWidth, 3), np.uint8)
-    xOffset = int((newImg.shape[1] - img.shape[1]) / 2)
-    yOffset = int((newImg.shape[0] - img.shape[0]) / 2)
-    newImg[yOffset:img.shape[0] + yOffset,
-           xOffset:img.shape[1] + xOffset] = img
-    return newImg
+def resize_image(img):
+    width_factor = screen_width / img.shape[1]
+    height_factor = screen_height / img.shape[0]
+    resize_factor = width_factor
+    if height_factor < width_factor:
+        resize_factor = height_factor
+    new_img = cv2.resize(img,
+                         (0, 0),
+                         fx=resize_factor,
+                         fy=resize_factor)                         
+    return new_img
+
+
+def frame_image(img):
+    new_img = np.zeros((screen_height, screen_width, 3), np.uint8)
+    x_offset = int((new_img.shape[1] - img.shape[1]) / 2)
+    y_offset = int((new_img.shape[0] - img.shape[0]) / 2)
+    new_img[y_offset:img.shape[0] + y_offset,
+           x_offset:img.shape[1] + x_offset] = img           
+    return new_img
+
+
+images = glob.glob("D:\\testImg\\2\\*.jpg")
+#images = glob.glob("C:\\Users\\Thomas\\Desktop\\photoframe\\*.jpg")
+count = len(images)
+
+if count == 0:
+    exit();
 
 cv2.namedWindow('window', cv2.WINDOW_NORMAL)
-cv2.setWindowProperty('window', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-x, y, sWidth, sHeight = cv2.getWindowImageRect('window')
-#images = glob.glob("D:\\testImg\\2\\*.jpg")
-images = glob.glob("C:\\Users\\Thomas\\Desktop\\photoframe\\*.jpg")
+cv2.setWindowProperty('window',
+                       cv2.WND_PROP_FULLSCREEN,
+                       cv2.WINDOW_FULLSCREEN)
+x, y, screen_width, screen_height = cv2.getWindowImageRect('window')
+i = 1;
+display_time = 2000
+fade_time = 3000
+fade_steps = int(fade_time / 100)
+time_per_fade_step = int(fade_time / fade_steps)
 
 image1 = cv2.imread(images[0])
-image1 = resizeImage(image1)
-image1 = frameImage(image1)
+image1 = resize_image(image1)
+image1 = frame_image(image1)
 cv2.imshow("window", image1)
-
-i = 1;
-count = len(images)
-displayTime = 2000
-fadeTime = 3000
-fadeSteps = int(fadeTime / 100)
-timePerFadeStep = int(fadeTime / fadeSteps)
 
 while True:
     if i == count:
-        i = 0
-        
+        i = 0        
     image2 = cv2.imread(images[i])
-    image2 = resizeImage(image2)
-    image2 = frameImage(image2)
+    image2 = resize_image(image2)
+    image2 = frame_image(image2)
+    blend_img = image1
     
-    cv2.waitKey(displayTime)
+    cv2.waitKey(display_time)
     
-    for a in range(fadeSteps - 1, 0, -1):
-        alpha = a / int(fadeTime / 100)
-        blendImg = image1
-        beta = ( 1.0 - alpha )
-        cv2.addWeighted(image1, alpha, image2, beta, 0.0, blendImg)
-        cv2.imshow("window", blendImg)
-        cv2.waitKey(timePerFadeStep)
+    # Blending images
+    for a in range(fade_steps - 1, 0, -1):
+        alpha = a / fade_steps
+        beta = ( 1.0 - alpha )     
+        cv2.addWeighted(image1, alpha,
+                        image2, beta,
+                        0.0, blend_img)
+        cv2.imshow("window", blend_img)
+        cv2.waitKey(time_per_fade_step)
         
     image1 = image2
     cv2.imshow("window", image1)
-    #cv2.waitKey(0)
     i += 1
